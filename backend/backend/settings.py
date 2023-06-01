138f859de4f520dcdc288596693c9620226d2f16
Roman Inozemtsev <dev@koyote92.ru>
2023-06-02 01:16:29 +0500

    little change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['koyote92.hopto.org', '158.160.12.5', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['koyote92.hopto.org', '84.39.241.83', '127.0.0.1', 'localhost']
 
 
 # Application definition
